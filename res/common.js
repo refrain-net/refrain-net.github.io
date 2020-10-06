e7d761f4af4209f01ae6e757c0a82dad43d7e8f9
refrain-net <72150044+refrain-net@users.noreply.github.com>
2020-10-07 07:53:39 +0900

    Add files via upload

---
+++ res/common.js
@@ -0,0 +1,34 @@
+'use strict';
+function createListItem (key) {
+  const { category = 'other', image = '/res/favicon.png', title = '', url = '' } = this[key];
+  const element = (`<section class = 'W1JhWxuv'>
+  <a category = ${category} class = 'TVOxHpZ9' href = ${url === '' ? `/article/?category=${category}&key=${key}` : url} target = '_new'>
+    <span class = 'eFcynxxU'>${title}</span>
+    <div class = 'RaWchbnJ'>
+      <img class = 'WaWu7Va9' src = ${image} />
+    </div>
+  </a>
+</section>`).toElement();
+  c6JO6k62.prependChild(element);
+}
+function dismissDialog () {
+  MsDGo7Eg.style.display = 'none';
+  m7eOG5wP.style.animation = 'none';
+}
+mWYXHFlu.addEventListener('keydown', event => {
+  switch (event.keyCode) {
+    case 13:
+      location.href = `/?keyword=${mWYXHFlu.value}`;
+      event.preventDefault();
+      break;
+  }
+});
+SYaLdKxJ.addEventListener('click', event => location.href = `/?keyword=${mWYXHFlu.value}`);
+const categories = {
+  article: '記事',
+  other: 'その他',
+  pixiv: 'Pixiv',
+  service: 'サービス',
+  youtube: 'YouTube'
+};
+if (/ip(one|(a|o)d)|android.*mobile/i.test(navigator.userAgent)) document.documentElement.setAttribute('sp', true);
---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+const { all = '', keyword = '' } = document.getQueryParameters();
+let ref = database.ref('article');
+keyword === ''
+    ? all === '' && (ref = ref.limitToLast(4))
+    : YFoSPTA3..appendChild((`<li class = 'q6kpK1jH'>
+  <a class = 'TnBfB6P0' href = '/?keyword=${keyword}'>
+    <span>検索結果: ${keyword}</span>
+  </a>
+</li>`).toElement());
+ref.once('value', snapshot => {
+  const val = snapshot.val() ?? { };
+  Object.keys(val).filter(key => (keyword === '' || String(val[key].title).includes(keyword))).forEach(createListItem, val);
+  dismissDialog();
+}).catch(console.error);
