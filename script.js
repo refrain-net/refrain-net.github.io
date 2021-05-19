b17b647119b96c4ff07f372057dbc25f1bca4e37
Refrain <72150044+refrain-net@users.noreply.github.com>
2021-05-19 21:39:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-'use strict';
-const {all = '', keyword = ''} = document.getParameters();
-let ref = database.ref('article');
-if (keyword !== '') YFoSPTA3.appendChild((`<li class = 'q6kpK1jH'>
-  <a class = 'TnBfB6P0' href = '/?keyword=${keyword}'>
-    <span>検索結果: ${keyword}</span>
-  </a>
-</li>`).toElement());
-else if (all === '') ref = ref.limitToLast(4);
-ref.once('value', snapshot => {
+(async () => {
+  'use strict';
+  const {all = '', keyword = ''} = document.getParameters();
+  let ref = database.ref('article');
+  if (keyword !== '') YFoSPTA3.appendChild((`<li class = 'q6kpK1jH'>
+    <a class = 'TnBfB6P0' href = '/?keyword=${keyword}'>
+      <span>検索結果: ${keyword}</span>
+    </a>
+  </li>`).toElement());
+  else if (all === '') ref = ref.limitToLast(4);
+  const snapshot = await ref.once('value');
   const val = snapshot.val() ?? {};
   Object.keys(val).filter(key => (keyword === '' || String(val[key].title).includes(keyword))).forEach(createListItem, val);
   dismissDialog();
-}).catch(console.error);
+})();
