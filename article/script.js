4673c91fdbb8bec6ef3bbc775a39a8d10e81df95
Refrain <72150044+refrain-net@users.noreply.github.com>
2021-05-19 21:44:24 +0900

    Update script.js

--- article/script.js
+++ article/script.js
@@ -1,11 +1,12 @@
-'use strict';
-const {category = '', key = ''} = document.getParameters();
-if (!(category in categories)) location.href = '/';
-YFoSPTA3.appendChild((`<li class = 'q6kpK1jH'>
-  <a class = 'TnBfB6P0' href = '/article/?category=${category}'>${categories[category]}</a>
-</li>`).toElement());
-const ref = key === '' ? database.ref('article').orderByChild('category').equalTo(category).limitToLast(4) : database.ref(`article/${key}`);
-ref.once('value', snapshot => {
+(async () => {
+  'use strict';
+  const {category = '', key = ''} = document.getParameters();
+  if (!(category in categories)) location.href = '/';
+  YFoSPTA3.innerHTML += `<li class = 'q6kpK1jH'><a class = 'TnBfB6P0' href = '/article/?category=${category}'>${categories[category]}</a></li>`;
+  const ref = key === '' ?
+      database.ref('article').orderByChild('category').equalTo(category).limitToLast(4) :
+      database.ref(`article/${key}`);
+  const snapshot = await ref.once('value');
   const val = snapshot.val() ?? {};
   if (key === '') Object.keys(val).forEach(createListItem, val);
   else {
@@ -18,4 +19,4 @@ ref.once('value', snapshot => {
     }
   }
   dismissDialog();
-});
+})();
