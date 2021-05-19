691585aba4b5819a819204a6f790dfa924d50664
Refrain <72150044+refrain-net@users.noreply.github.com>
2021-05-19 21:50:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,7 @@
   'use strict';
   const {all = '', keyword = ''} = document.getParameters();
   let ref = database.ref('article');
-  if (keyword !== '') YFoSPTA3.appendChild((`<li class = 'q6kpK1jH'>
-    <a class = 'TnBfB6P0' href = '/?keyword=${keyword}'>
-      <span>検索結果: ${keyword}</span>
-    </a>
-  </li>`).toElement());
+  if (keyword !== '') YFoSPTA3.innerHTM += `<li class = 'q6kpK1jH'><a class = 'TnBfB6P0' href = '/?keyword=${keyword}'><span>検索結果: ${keyword}</span></a></li>`;
   else if (all === '') ref = ref.limitToLast(4);
   const snapshot = await ref.once('value');
   const val = snapshot.val() ?? {};
